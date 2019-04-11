4a0616278d616714608d96f6c109eb76a9fc4a61
jessale <florian.schneller@gmx.de>
2019-04-11 19:46:59 +0200

    basic structure

--- src/desktop.js
+++ src/desktop.js
@@ -1,5 +1,5 @@
 let documents = document.createElement("DIV");
-documents.setAttribute("class", "icon");
+documents.setAttribute("class", "icons");
 documents.setAttribute("draggable", "true");
 documents.innerHTML = '<p class="text"><span>MyProjects</span></p>';
 documents.style.background ="url(img/folder.png) center top no-repeat";
@@ -7,7 +7,7 @@ desktopicons = document.getElementById("desktop-icons");
 desktopicons.append(documents);
 
 let mail = document.createElement("DIV");
-mail.setAttribute("class", "icon");
+mail.setAttribute("class", "icons");
 mail.setAttribute("draggable", "true", "onclick", "calender()");
 mail.innerHTML = '<p class="text"><span>Email me</span></p>';
 mail.style.background ="url(img/mail.png) center top no-repeat";
@@ -15,9 +15,15 @@ desktopicons = document.getElementById("desktop-icons");
 desktopicons.append(mail);
 
 let legal = document.createElement("DIV");
-legal.setAttribute("class", "icon");
+legal.setAttribute("class", "icons");
 legal.setAttribute("draggable", "true");
+legal.setAttribute("id", "legal")
 legal.innerHTML = '<p class="text"><span>Legal.docx</span></p>';
 legal.style.background ="url(img/word.png) center top no-repeat";
 desktopicons = document.getElementById("desktop-icons");
 desktopicons.append(legal);
+
+$("#legal").click(function() {
+  $("#explorer").css("visibility", "visible");
+  $(".tasks").append("<div id='task'><p class='task_text'><span>Game</span></p></div>");
+});
