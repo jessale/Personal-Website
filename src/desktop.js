d38ae15b1d77827c07952e8829b2454a9d3dc47e
jessale <florian.schneller@gmx.de>
2019-04-30 18:42:12 +0200

    ooj

--- src/desktop.js
+++ src/desktop.js
@@ -1,3 +1,21 @@
+function Deskicon(id, name, iconurl){
+  this.id = id;
+  this.name = name;
+  this.iconurl = iconurl;
+  name = document.createElement('DIV');
+  name.setAttribute("class", "icons");
+  name.setAttribute("draggable", "true");
+  name.setAttribute("id", `${this.id}`);
+  name.innerHTML = `<p class="text"><span>${this.name}</span></p>`;
+  name.style.background =`url(${this.iconurl}) center top no-repeat`;
+  desktopicons = document.getElementById("desktop-icons");
+  desktopicons.append(name);
+}
+
+const documents = new Deskicon('documents', 'MyProjects','img/folder.png')
+const mail2 = new Deskicon('mail', 'Email', 'img/mail.png')
+const legal = new Deskicon('legal', 'Legal.docx', 'img/word.png')
+/*
 let documents = document.createElement("DIV");
 documents.setAttribute("class", "icons");
 documents.setAttribute("draggable", "true");
@@ -5,24 +23,8 @@ documents.setAttribute("id", "documents");
 documents.innerHTML = '<p class="text"><span>MyProjects</span></p>';
 documents.style.background ="url(img/folder.png) center top no-repeat";
 desktopicons = document.getElementById("desktop-icons");
-desktopicons.append(documents);
+desktopicons.append(documents); */
 
-let mail = document.createElement("DIV");
-mail.setAttribute("class", "icons");
-mail.setAttribute("draggable", "true", "onclick", "calender()");
-mail.innerHTML = '<p class="text"><span>Email me</span></p>';
-mail.style.background ="url(img/mail.png) center top no-repeat";
-desktopicons = document.getElementById("desktop-icons");
-desktopicons.append(mail);
-
-let legal = document.createElement("DIV");
-legal.setAttribute("class", "icons");
-legal.setAttribute("draggable", "true");
-legal.setAttribute("id", "legal")
-legal.innerHTML = '<p class="text"><span>Legal.docx</span></p>';
-legal.style.background ="url(img/word.png) center top no-repeat";
-desktopicons = document.getElementById("desktop-icons");
-desktopicons.append(legal);
 
 $("#legal").click(function() {
   word();
